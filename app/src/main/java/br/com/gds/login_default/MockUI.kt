e57199d5bd00ff908009292c20f00c96ab77304a
Gabriel Alves <gcarmodev@gdsdesenvolvimento.com>
2024-12-14 23:36:37 -0300

    feat: (layout) refactor layout login v0

--- app/src/main/java/br/com/gds/login_default/MockUI.kt
+++ app/src/main/java/br/com/gds/login_default/MockUI.kt
@@ -11,15 +11,37 @@ import br.com.gds.login.utils.commons.LayoutSetup
 object MockUI {
     private val layoutDefault = LayoutDefault(
         isStatusBarEnabled = true,
-        statusBarColor = R.color.black,
+        statusBarColor = R.color.teste_one,
         isNavigationBarEnabled = true,
-        navigationBarColor = R.color.black
+        navigationBarColor = R.color.teste_two
     )
-    private val registerFragment = RegisterPersonalUI()
-    private val addressRegisterFragment = AddressRegisterUI()
-    private val autoRegisterFragment = AutomovelRegisterUI()
-    private val loginFragment = LoginUI()
-    private val resetPasswordFragment = ResetPasswordUI()
+
+    private val registerFragment = RegisterPersonalUI(
+        enableButtonAddress = false,
+        enableNickname = false,
+        //        backgroundColor = 0,
+        //        titleColor = 0
+    )
+
+    private val addressRegisterFragment = AddressRegisterUI(
+        //        backgroundColor = 0,
+        //        titleColor = 0
+    )
+
+    private val autoRegisterFragment = AutomovelRegisterUI(
+        //        backgroundColor = 0,
+        //        titleColor = 0
+    )
+
+    private val loginFragment = LoginUI(
+        //        backgroundColor = 0,
+        //        titleColor = 0
+    )
+
+    private val resetPasswordFragment = ResetPasswordUI(
+        //        backgroundColor = 0,
+        //        titleColor = 0
+    )
 
     val layoutSetup = LayoutSetup(
         layoutDefault = layoutDefault,
--- login/src/main/java/br/com/gds/login/feature/login/view/LoginFragment.kt
+++ login/src/main/java/br/com/gds/login/feature/login/view/LoginFragment.kt
@@ -1,26 +1,16 @@
 package br.com.gds.login.feature.login.view
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import br.com.gds.login.LoginModuleSession
-import br.com.gds.login.R
 import br.com.gds.login.databinding.FragmentLoginBinding
 import br.com.gds.login.feature.login.model.LoginUI
-import br.com.gds.login.feature.login.model.UserLogin
 import br.com.gds.login.feature.login.viewmodel.LoginViewModel
-import br.com.gds.login.utils.extensions.edittext.EditTextState
-import br.com.gds.login.utils.extensions.edittext.applyStyle
-import br.com.gds.login.utils.extensions.edittext.validateEmailField
-import br.com.gds.login.utils.extensions.edittext.validatePasswordField
-import br.com.gds.login.utils.extensions.toastMessage
-import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
 
@@ -45,166 +35,166 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
-    @SuppressLint("ResourceAsColor")
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+//    @SuppressLint("ResourceAsColor")
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        fragmentUI.let {
+//            binding.loginContainerScroll.setBackgroundColor(
+//                requireContext().getColor(it.backgroundColor)
+//            )
+//            binding.loginTextTitle.setTextColor(
+//                requireContext().getColor(it.titleColor)
+//            )
+//        }
+//        setupListeners()
+//        fieldObserve()
+//        viewModelObserve()
+//    }
+//
+//    private fun setupListeners() = binding.apply {
+//        loginBtnRegister.setOnClickListener {
+//            register()
+//            toastMessage("Clique btn register")
+//        }
+//        loginBtnResetPassword.setOnClickListener {
+//            resetPassword()
+//            toastMessage("Clique btn reset password")
+//        }
+//        loginBtnEnterApp.setOnClickListener {
+//            viewModel.login(
+//                userLogin = getUserLogin()
+//            )
+//        }
+//        loginCkRemember.setOnCheckedChangeListener { _, check ->
+//            if (check) viewModel.rememberDataUser() else viewModel.removeDataUser()
+//            toastMessage("Clique checkbox")
+//        }
+//        providersListeners()
+//    }
+//
+//    private fun getUserLogin() = UserLogin(
+//        email = binding.loginEmailEdit.text.toString(),
+//        password = binding.loginPasswordEdit.text.toString(),
+//        isRemember = binding.loginCkRemember.isChecked
+//    )
+//
+//    private fun resetPassword() {
+//      navController.navigate(
+//          LoginFragmentDirections.actionLoginFragmentToResetPasswordFragment2()
+//      )
+//    }
+//
+//    private fun register() {
+//        navController.navigate(
+//            LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+//        )
+//    }
 
-        fragmentUI.let {
-            binding.loginContainerScroll.setBackgroundColor(
-                requireContext().getColor(it.backgroundColor)
-            )
-            binding.loginTextTitle.setTextColor(
-                requireContext().getColor(it.titleColor)
-            )
-        }
-        setupListeners()
-        fieldObserve()
-        viewModelObserve()
-    }
-
-    private fun setupListeners() = binding.apply {
-        loginBtnRegister.setOnClickListener {
-            register()
-            toastMessage("Clique btn register")
-        }
-        loginBtnResetPassword.setOnClickListener {
-            resetPassword()
-            toastMessage("Clique btn reset password")
-        }
-        loginBtnEnterApp.setOnClickListener {
-            viewModel.login(
-                userLogin = getUserLogin()
-            )
-        }
-        loginCkRemember.setOnCheckedChangeListener { _, check ->
-            if (check) viewModel.rememberDataUser() else viewModel.removeDataUser()
-            toastMessage("Clique checkbox")
-        }
-        providersListeners()
-    }
-
-    private fun getUserLogin() = UserLogin(
-        email = binding.loginEmailEdit.text.toString(),
-        password = binding.loginPasswordEdit.text.toString(),
-        isRemember = binding.loginCkRemember.isChecked
-    )
-
-    private fun resetPassword() {
-      navController.navigate(
-          LoginFragmentDirections.actionLoginFragmentToResetPasswordFragment2()
-      )
-    }
-
-    private fun register() {
-        navController.navigate(
-            LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
-        )
-    }
-
-    private fun providersListeners() = binding.apply {
-        googleProvider.setOnClickListener {
-            toastMessage("Clique google")
-        }
-        appleProvider.setOnClickListener {
-            toastMessage("Clique apple")
-        }
-        facebookProvider.setOnClickListener {
-            toastMessage("Clique facebook")
-        }
-        phoneProvider.setOnClickListener {
-            toastMessage("Clique phone")
-        }
-        guestsProvider.setOnClickListener {
-            toastMessage("Clique guests")
-        }
-    }
-
-    private fun fieldObserve() {
-        lifecycleScope.launch {
-            binding.loginEmailEdit.validateEmailField().collect { state ->
-                when (state) {
-                    is EditTextState.Empty -> emailEmptyStyle()
-                    is EditTextState.Invalid -> emailInvalidStyle(state)
-                    is EditTextState.Valid -> emailValidStyle()
-                }
-                enabledButton()
-            }
-        }
-        lifecycleScope.launch {
-            binding.loginPasswordEdit.validatePasswordField().collect { state ->
-                when (state) {
-                    is EditTextState.Empty -> passwordEmptyStyle()
-                    is EditTextState.Invalid -> passwordInvalidStyle(state)
-                    is EditTextState.Valid -> passwordValidStyle()
-                }
-                enabledButton()
-            }
-        }
-    }
-
-    private fun enabledButton() = binding.loginBtnEnterApp.apply {
-        isEnabled = email && password
-    }
-
-    private fun viewModelObserve() {
-        lifecycleScope.launch {
-            viewModel.uiState.collect { state ->
+//    private fun providersListeners() = binding.apply {
+//        googleProvider.setOnClickListener {
+//            toastMessage("Clique google")
+//        }
+//        appleProvider.setOnClickListener {
+//            toastMessage("Clique apple")
+//        }
+//        facebookProvider.setOnClickListener {
+//            toastMessage("Clique facebook")
+//        }
+//        phoneProvider.setOnClickListener {
+//            toastMessage("Clique phone")
+//        }
+//        guestsProvider.setOnClickListener {
+//            toastMessage("Clique guests")
+//        }
+//    }
+//
+//    private fun fieldObserve() {
+//        lifecycleScope.launch {
+//            binding.loginEmailEdit.validateEmailField().collect { state ->
 //                when (state) {
-//                    is LoginUIState -> {}
-//                    is LoginUIState.Loading -> {}
-//                    is LoginUIState.Success -> {}
+//                    is EditTextState.Empty -> emailEmptyStyle()
+//                    is EditTextState.Invalid -> emailInvalidStyle(state)
+//                    is EditTextState.Valid -> emailValidStyle()
 //                }
-            }
-        }
-    }
-
-    private fun passwordInvalidStyle(state: EditTextState.Invalid) {
-        binding.loginPasswordLayout.apply {
-            error = state.errorMessage
-            applyStyle(R.style.TextInputLayout_Invalid)
-        }
-        password = false
-    }
-
-    private fun passwordValidStyle() {
-        binding.loginPasswordLayout.apply {
-            error = null
-            applyStyle(R.style.TextInputLayout_Valid)
-        }
-        password = true
-    }
-
-    private fun passwordEmptyStyle() {
-        binding.loginPasswordLayout.apply {
-            error = null
-            applyStyle(R.style.TextInputLayout_Empty)
-        }
-        password = false
-    }
-
-    private fun emailValidStyle() {
-        binding.loginEmailLayout.apply {
-            error = null
-            applyStyle(R.style.TextInputLayout_Valid)
-        }
-        email = true
-    }
-
-    private fun emailInvalidStyle(state: EditTextState.Invalid) {
-        binding.loginEmailLayout.apply {
-            error = state.errorMessage
-            applyStyle(R.style.TextInputLayout_Invalid)
-        }
-        email = false
-    }
-
-    private fun emailEmptyStyle() {
-        binding.loginEmailLayout.apply {
-            error = null
-            applyStyle(R.style.TextInputLayout_Empty)
-        }
-        email = false
-    }
+//                enabledButton()
+//            }
+//        }
+//        lifecycleScope.launch {
+//            binding.loginPasswordEdit.validatePasswordField().collect { state ->
+//                when (state) {
+//                    is EditTextState.Empty -> passwordEmptyStyle()
+//                    is EditTextState.Invalid -> passwordInvalidStyle(state)
+//                    is EditTextState.Valid -> passwordValidStyle()
+//                }
+//                enabledButton()
+//            }
+//        }
+//    }
+//
+//    private fun enabledButton() = binding.loginBtnEnterApp.apply {
+//        isEnabled = email && password
+//    }
+//
+//    private fun viewModelObserve() {
+//        lifecycleScope.launch {
+//            viewModel.uiState.collect { state ->
+////                when (state) {
+////                    is LoginUIState -> {}
+////                    is LoginUIState.Loading -> {}
+////                    is LoginUIState.Success -> {}
+////                }
+//            }
+//        }
+//    }
+//
+//    private fun passwordInvalidStyle(state: EditTextState.Invalid) {
+//        binding.loginPasswordLayout.apply {
+//            error = state.errorMessage
+//            applyStyle(R.style.TextInputLayout_Invalid)
+//        }
+//        password = false
+//    }
+//
+//    private fun passwordValidStyle() {
+//        binding.loginPasswordLayout.apply {
+//            error = null
+//            applyStyle(R.style.TextInputLayout_Valid)
+//        }
+//        password = true
+//    }
+//
+//    private fun passwordEmptyStyle() {
+//        binding.loginPasswordLayout.apply {
+//            error = null
+//            applyStyle(R.style.TextInputLayout_Empty)
+//        }
+//        password = false
+//    }
+//
+//    private fun emailValidStyle() {
+//        binding.loginEmailLayout.apply {
+//            error = null
+//            applyStyle(R.style.TextInputLayout_Valid)
+//        }
+//        email = true
+//    }
+//
+//    private fun emailInvalidStyle(state: EditTextState.Invalid) {
+//        binding.loginEmailLayout.apply {
+//            error = state.errorMessage
+//            applyStyle(R.style.TextInputLayout_Invalid)
+//        }
+//        email = false
+//    }
+//
+//    private fun emailEmptyStyle() {
+//        binding.loginEmailLayout.apply {
+//            error = null
+//            applyStyle(R.style.TextInputLayout_Empty)
+//        }
+//        email = false
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
